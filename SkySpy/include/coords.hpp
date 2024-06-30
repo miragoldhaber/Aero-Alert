09aa8d24d5fd64ad08a23aad0fded7f9016917be
mira <116974840+miragoldhaber@users.noreply.github.com>
2024-06-30 11:16:53 -0400

    add: code within coord.hpp copied from rishyaks example of GPS Adafruit IO

--- SkySpy/include/coords.hpp
+++ SkySpy/include/coords.hpp
@@ -1,5 +1,20 @@
 #ifndef COORD_HPP
 #define COORD_HPP
+struct coord {
+  double latitude;
+  char lat;
+  double longitude;
+  char lon;
+  double altitude;
 
+  coord() = default;
+
+  coord(double latitude, char lat, double longitude, char lon, double altitude)
+      : latitude(latitude),
+        lat(lat),
+        longitude(longitude),
+        lon(lon),
+        altitude(altitude) {}
+};
 
 #endif //COORD_HPP
