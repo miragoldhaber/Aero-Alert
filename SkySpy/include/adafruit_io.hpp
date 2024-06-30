ee988ac7f0d62c72ef83d5c03908ed9b133b1c8d
mira <116974840+miragoldhaber@users.noreply.github.com>
2024-06-30 11:20:38 -0400

    add: wifi defns and include for adafruit_io.hpp NOTE: THIS IS ON MY (mira's) adafruit io account & home wifi, will change wifi later

--- SkySpy/include/adafruit_io.hpp
+++ SkySpy/include/adafruit_io.hpp
@@ -1,6 +1,17 @@
 // add to the sky spy 2
 #ifndef ADAFRUIT_IO_HPP
 #define ADAFRUIT_IO_HPP
+//get my key
+#define IO_USERNAME "mira_g"
+#define IO_KEY "aio_Wbaf05ckxTjPqCBfWpvnaueSNvCt"
+//add whichever wifi I use + password
+// figure out how it can work in diff places
+#define WIFI_SSID "Fios-Q3R6Q"
+#define WIFI_PASS "ron8044fund856aim"
+
+#include <AdafruitIO_WiFi.h>
+#include <tempsensor.hpp>
+#include <GPS.hpp>
 
 
 
