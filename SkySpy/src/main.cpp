a5d28dc16d4007b7aebcc9ee6f74df1fc451ef6b
Megan Cheng <mcheng-27@peddie.org>
2024-07-01 11:00:32 -0400

    updated main

--- SkySpy/src/main.cpp
+++ SkySpy/src/main.cpp
@@ -1,14 +1,29 @@
-#include <SPI.h>
 #include <Arduino.h>
-#include <Wire.h>
-#include <gps.hpp>
+#include <tempsensor.hpp>
+#include <adafruit_io.hpp>
+#include <screen.hpp>
+#include <GPS.hpp>
+#include <coord.hpp>
 
-void setup() {
-  // put your setup code here, to run once:
-  gps::gps_setup();
+void setup(){
+  Serial.begin(115200);
+  while(!Serial);
+  temperature::prepare();
+  gps::setup();
+  adafruit_io::attempt_connection();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  gps::gps_loop();
+  gps::loop();
+  temperature::display();
+  adafruit_io::run();
+  delay(5000);
+
+  if (adafruit_io::IO_connected) {
+  adafruit_io::save_to_IOT();
+  }
+
+  // if time add feed for the date
+  // see if they can be connected -- make sure they aren't all just individual
+  //figure out all of coordinates stuff once i have megans code
+  // maybe make this in another file (io save or smth), bc main is getting long
 }
